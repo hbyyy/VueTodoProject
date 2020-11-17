a1066b8e9206cede426675e3529aa72a93a3b22c
hbyyy <qjaduddl94@gmail.com>
2020-11-18 00:06:29 +0900

    feat: add button 커스텀

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <input type="text" v-model="newTodoItem">
-        <button>추가</button>
+        <input type="text" v-model="newTodoItem" placeholder="할 일을 추가하세요" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+        </span>
     </div>
 </template>
 
@@ -10,6 +12,18 @@ export default {
     data() {
         return {
             newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodoItem !== ''){
+                var value = this.newTodoItem && this.newTodoItem.trim();
+                localStorage.setItem(value, value);
+                this.clearInput();
+            }
+        },
+        clearInput() {
+            this.newTodoItem = '';
         }
     }
 }
