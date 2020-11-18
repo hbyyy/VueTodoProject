75ef38f1bee7d578f78881f3c06380ccd7f3b50f
hbyyy <qjaduddl94@gmail.com>
2020-11-19 00:42:28 +0900

    feat: Todo 완성

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
-    <ul>
-      <li v-for="(todoItem, index) in propsdata" :key="index" class="Shadow">
+    <transition-group name="list" tag="ul">
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="index" class="Shadow">
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
         {{ todoItem }}
         <span
@@ -12,22 +12,29 @@
           <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['propsdata'],
+  props: ["propsdata"],
   methods: {
     removeTodo(todoItem, index) {
-      this.$emit('removeTodo', todoItem, index);
+      this.$emit("removeTodo", todoItem, index);
     },
   },
 };
 </script>
 
 <style scoped>
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 ul {
   list-style-type: none;
   padding-left: 0px;
